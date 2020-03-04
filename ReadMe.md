3ecbbf785e8ae8860cd5a79065a08e59e8f95f66
Guessbean <guessbean@gmail.com>
2020-03-05 00:03:10 +0100

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+# Setting up NGINX with Docker
---
+++ scripts/setupRepository.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# Copy the files from the NGINX Docker container to the local repository
+cd ../shared
+docker build -t nginx_production ../docker
+id=$(docker run --rm --name nginx_cp -p 80:80 -d nginx_production)
+docker cp $id:/etc/nginx ./etc/nginx
+docker cp $id:/usr/share/nginx/html/ ./src/usr/share/nginx/html
+docker stop $id
