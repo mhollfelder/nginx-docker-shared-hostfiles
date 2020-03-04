696c41ee0b32028022c5ef934bfcc96b16a9df3d
Guessbean <guessbean@gmail.com>
2020-03-05 00:13:43 +0100

    Changed setup script

--- scripts/setupRepository.sh
+++ scripts/setupRepository.sh
@@ -4,6 +4,8 @@
 cd ../shared
 docker build -t nginx_production ../docker
 id=$(docker run --rm --name nginx_cp -p 80:80 -d nginx_production)
+mkdir -p ./etc/nginx
+mkdir -p ./src/usr/share/nginx/html
 docker cp $id:/etc/nginx ./etc/nginx
 docker cp $id:/usr/share/nginx/html/ ./src/usr/share/nginx/html
 docker stop $id
