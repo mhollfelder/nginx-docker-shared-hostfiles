f6b276b10ac4c7df49d27318d04752c554a10fd9
Guessbean <guessbean@gmail.com>
2020-03-05 00:51:53 +0100

    Fixing paths

--- scripts/setupRepository.sh
+++ scripts/setupRepository.sh
@@ -5,9 +5,9 @@ cd ../shared
 docker build -t nginx_production ../docker
 id=$(docker run --rm --name nginx_cp -p 80:80 -d nginx_production)
 mkdir -p ./etc/nginx
-mkdir -p ./src/usr/share/nginx/html
+mkdir -p ./usr/share/nginx/html
 docker cp $id:/etc/nginx ./etc/nginx
-docker cp $id:/usr/share/nginx/html/ ./src/usr/share/nginx/html
+docker cp $id:/usr/share/nginx/html/ ./usr/share/nginx/html
 docker stop $id
 
 # From here, we can continue with configuring the files
