d8cabd37f2ab660156e2403918b1206e88ca1823
Guessbean <guessbean@gmail.com>
2020-03-05 01:22:37 +0100

    Fixed repository issues

--- scripts/setupRepository.sh
+++ scripts/setupRepository.sh
@@ -1,13 +1,25 @@
-#/bin/bash
+#!/bin/bash
+
+# Bash wrapper for the build environment
+
+# https://stackoverflow.com/a/246128
+# Get the directory from where the script is called for further path resolution
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+#
 
 # Copy the files from the NGINX Docker container to the local repository
-cd ../shared
-docker build -t nginx_production ../docker
+docker build -t nginx_production "${DIR}/../docker"
 id=$(docker run --rm --name nginx_cp -p 80:80 -d nginx_production)
-mkdir -p ./etc/nginx
-mkdir -p ./usr/share/nginx/html
-docker cp $id:/etc/nginx ./etc/nginx
-docker cp $id:/usr/share/nginx/html/ ./usr/share/nginx/html
+mkdir -p ${DIR}/../shared/etc/nginx
+mkdir -p ${DIR}/../shared/nginx/html
+docker cp $id:/etc/nginx ${DIR}/../shared/etc/nginx
+docker cp $id:/usr/share/nginx/html/ ${DIR}/../shared/usr/share/nginx/html
 docker stop $id
 
 # From here, we can continue with configuring the files
